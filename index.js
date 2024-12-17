c02cc8aedd4a22510262184470eaed35da9f8878
MeEtSoNi-lab <meet@tulong.tech>
2024-12-17 16:31:22 -0500

    Added route languageconverse

--- index.js
+++ index.js
@@ -96,6 +96,18 @@ app.get('/traveledcountries', async (req, res) => {
     }
   });
 
+// Fetch all languageconverse
+app.get('/languageconverse', async (req, res) => {
+    try {
+      const alllanguageconverse = await languageconverse.find(); // Await the asynchronous operation
+      console.log(alllanguageconverse)
+      res.status(200).json(alllanguageconverse);
+    } catch (error) {
+      console.error('Error fetching languageconverse:', error);
+      res.status(500).json({ message: 'Failed to fetch languageconverse.' });
+    }
+  });
+
 
 // Calculate sum
 app.post('/calculate', (req, res) => {
